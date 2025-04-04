b2ffb3c7f1f92bee20240eae81af34cbd9c8fd4d
monclair <gabrielcazevedo19@gmail.com>
2025-04-03 21:26:21 -0300

    2

--- 27032025/Exercício 3.py
+++ 27032025/Exercício 3.py
@@ -1,11 +1,15 @@
 matéria1 = float(input('Digite a primeira nota:'))
 matéria2 = float(input('Digite a segunda nota:'))
 matéria3 = float(input('Digite a terceira nota:'))
-d = (matéria1 + matéria2 + matéria3) / 3
-e = d > 7
+e = matéria1 > 7
+f = matéria2 > 7
+g = matéria3 > 7
+
 print('='*50)
 print('True = você foi aprovado, False = você foi reprovado',)
 print('='*50)
 print(e)
+print(f)
+print(g)
 print('='*50)
 
--- 27032025/Exercício 4.py
+++ 27032025/Exercício 4.py
@@ -1,4 +1,7 @@
-a = int(input('Digite um número:'))
-b = int(input('Digite outro número:'))
-c = a + b
-print('A soma desses números é', c)
+try:
+    a = int(input('Digite um número:'))
+    b = int(input('Digite outro número:'))
+    c = a + b
+    print('A soma desses números é', c)
+except (ValueError):
+    print('Você não digitou um número')
