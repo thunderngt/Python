a403be89ee32f4ce7ea50a78e95339a144171c9e
monclair <gabrielcazevedo19@gmail.com>
2025-04-10 22:09:50 -0300

    1

---
+++ 10042025/Exercício 1.py
@@ -0,0 +1,15 @@
+try:
+    lista = [1, 2, 3, 4, 5]
+    list = [0.5, 1, 4, 7, 8]
+    valor = 0.0
+    code = int(input('Digite o código do produto comprado:'))
+    quant = int(input('Digite a quantidade do produto:'))
+    qtde = code * quant
+
+except:
+    print('Código inválido')
+
+for i in range [5]:
+    if code[i] == lista:
+        valor = list [i] * quant
+    print(list * quant)
